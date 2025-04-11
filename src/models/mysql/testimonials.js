17d7cecedf32a3f8c00b70e97dc04f5d7a876ce4
jhoancanchila <canchila.jhoan@gmail.com>
2025-04-11 11:37:35 -0500

    update conection model testimonials

--- src/models/mysql/testimonials.js
+++ src/models/mysql/testimonials.js
@@ -1,13 +1,15 @@
 const db = require("../../../db");
+const dbPostgres = require("../../../db-postgres");
 
 const getAll = async( ) => {
 
-  try {    
-    const [ response ] = await db.query(`SELECT * FROM TESTIMONIALS WHERE rating > 3`);
+  try {   
+    const response = await dbPostgres`SELECT * FROM "TESTIMONIALS" WHERE rating > 3`; 
+    /* const [ response ] = await db.query(`SELECT * FROM TESTIMONIALS WHERE rating > 3`); */
   
     return response;
   } catch (error) {
-    throw error;
+    throw new error;
   }
 
 }
